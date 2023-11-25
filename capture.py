10960771450e71ba039003f2d5f89175e01d49e3
bitbitlemon <136308066+bitbitlemon@users.noreply.github.com>
2023-11-25 21:21:15 +0800

    Update capture.py

--- capture.py
+++ capture.py
@@ -1,3 +1,13 @@
 #使用mss库可以进行截屏操作
 import mss
+import time
+# 创建 mss 的实例
+with mss.mss() as sct:
+    # 循环捕捉屏幕
+    while True:
+        # 获取屏幕截图
+        screenshot = sct.shot(output="screenshot.png") 
+        # 暂停一段时间，可以根据需要调整间隔时间
+        time.sleep(1)
+
 
