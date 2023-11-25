e95790232bdf7e06b77656f01898692b7b4e7e3a
bitbitlemon <136308066+bitbitlemon@users.noreply.github.com>
2023-11-25 21:22:39 +0800

    Update capture.py

--- capture.py
+++ capture.py
@@ -6,7 +6,7 @@ with mss.mss() as sct:
     # 循环捕捉屏幕
     while True:
         # 获取屏幕截图
-        screenshot = sct.shot(output="screenshot.png") 
+        screenshot = sct.shot(output="screenshot.png") #默认图片保存在当前目录
         # 暂停一段时间，可以根据需要调整间隔时间
         time.sleep(1)
 
